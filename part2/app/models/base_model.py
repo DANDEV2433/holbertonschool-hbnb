bac4bb488166b13fed87e4f1b878b8a2afa297d0
DANDEV2433 <alexdani3344@gmail.com>
2025-02-25 18:25:44 +0100

    add user.py and basemodel

---
+++ part2/app/models/base_model.py
@@ -0,0 +1,19 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())  # Identifiant unique
+        self.created_at = datetime.now()  # Horodatage de la création
+        self.updated_at = datetime.now()  # Horodatage de la dernière mise à jour
+
+    def save(self):
+        """Met à jour le timestamp `updated_at`."""
+        self.updated_at = datetime.now()
+
+    def update(self, data):
+        """Met à jour les attributs à partir d'un dictionnaire de nouvelles valeurs."""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()

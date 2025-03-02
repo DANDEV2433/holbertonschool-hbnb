3406e7501accbb4dd659381f1757adab9ab8c076
DANDEV2433 <alexdani3344@gmail.com>
2025-03-02 19:38:15 +0100

    import request in reviews

--- part2/app/api/v1/reviews.py
+++ part2/app/api/v1/reviews.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+from flask import request
 
 api = Namespace('reviews', description='Review operations')
 
